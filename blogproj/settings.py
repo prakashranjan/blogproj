28c8edb5563deff7b488e4ca2b7c24a434bc561d
prakashranjan <prakashranjansingh04@gmail.com>
2018-08-11 21:08:56 +0530

    completed run

--- blogproj/settings.py
+++ blogproj/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -72,13 +73,26 @@ WSGI_APPLICATION = 'blogproj.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
+#now for MYSQL database config connection
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogdjango',
+        'USER': 'root',
+        'PASSWORD': 'MAGICdragon@7856',
+        'HOST': 'localhost',
+        'PORT': ''
+    }
+}
+
 
 
 # Password validation
