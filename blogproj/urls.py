eed73439e715031fc7e75c289be83e9d5e07a45c
prakashranjan <prakashranjansingh04@gmail.com>
2018-08-25 20:32:50 +0530

    solving error at /

--- blogproj/urls.py
+++ blogproj/urls.py
@@ -18,7 +18,7 @@ from django.urls import path ,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/', include('posts.urls')),
-    path('',include('posts.urls') )
+    #path('posts/', include('posts.urls')),
+    #path('',include('posts.urls') )
 
 ]
